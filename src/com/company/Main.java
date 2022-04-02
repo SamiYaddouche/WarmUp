f999a69d0464b5249607018b1dff272f19e45f6c
GAME-PC\GAME <yaddouche.sami@fh-swf.de>
2022-04-02 16:04:01 +0200

    WarmUpAufgabe Version 1.0 Added pruefePrimzahl Methode
    Version 2.0
    Added berechneQuersumme Methode
    
    Version 3.0
    Added berechneAnzahlXistPrimzahlUndQuersummePrimzahl

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,9 +20,30 @@ public class Main {
 
         return quersumme;
     }
+
+    public static String berechneAnzahlXistPrimzahlUndQuersummeIstPrimzahl (int zahl) {
+       String ausgabe = "";
+        int anzahl = 0;
+
+        if(zahl >= 100 || zahl <=1) {
+           ausgabe = "Ungültig! \nZahl muss zwischen 1 und 100 sein.\nGeben Sie die Zahl erneut ein.";
+       }
+       if(zahl < 100 && zahl > 1) {
+           for (int i = 2; i <= zahl; i++) {
+
+               if (pruefeObPrimzahl(i) && pruefeObPrimzahl(berechneQuersumme(i)))
+                   anzahl++;
+               ausgabe = "Frage: Für wie viele Zahlen x zwischen 1 und 100 gilt:\nDie Zahl ist eine Primzahl und die Quersumme von der Primzahl ist ebenfalls eine Primzahl ?:\n\nAntwort: " + anzahl;
+           }
+       }
+        return ausgabe;
+
+    }
+
     public static void main(String[] args) {
 	    // System.out.println(pruefeObPrimzahl(4));
-        System.out.println(berechneQuersumme(112));
+        //System.out.println(berechneQuersumme(11));
+        System.out.print(berechneAnzahlXistPrimzahlUndQuersummeIstPrimzahl(23));
 
     }
 }
