165a274e1d54cbbd9588a4d5c11c6326bd124701
GAME-PC\GAME <yaddouche.sami@fh-swf.de>
2022-04-02 15:29:07 +0200

    WarmUpAufgabe Version 1.0 Added pruefePrimzahl Methode
    Version 2.0
    Added berechneQuersumme Methode

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,19 @@ public class Main {
         return ergebnis;
     }
 
+    public static int berechneQuersumme(int zahl) {
+        int quersumme = 0;
+        int rest = 0;
+        for (int i = 0; zahl > 0; i++){
+            quersumme += zahl%10;
+            zahl = zahl / 10;
+        }
+
+        return quersumme;
+    }
     public static void main(String[] args) {
-	System.out.println(pruefeObPrimzahl(4));
+	    // System.out.println(pruefeObPrimzahl(4));
+        System.out.println(berechneQuersumme(112));
+
     }
 }
