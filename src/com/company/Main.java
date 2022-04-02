12acc41e6a3e30966ebbf931acf1097d82a518c1
GAME-PC\GAME <yaddouche.sami@fh-swf.de>
2022-04-02 15:12:14 +0200

    WarmUpAufgabe Version 1.0 Added pruefePrimzahl Methode

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,15 @@ package com.company;
 
 public class Main {
 
+    public static boolean pruefeObPrimzahl (int zahl) {
+        boolean ergebnis = false;
+        if (zahl > 1 && zahl%2 != 0){
+            ergebnis = true;
+        }
+        return ergebnis;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+	System.out.println(pruefeObPrimzahl(4));
     }
 }
